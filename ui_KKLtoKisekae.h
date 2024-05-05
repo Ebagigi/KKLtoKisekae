bc991c8576705bce85e5125451138128b71bcc4d
Gabriel Misajlovski <62955382+Ebagigi@users.noreply.github.com>
2024-05-05 12:21:47 -0400

    Working UI file

--- ui_KKLtoKisekae.h
+++ ui_KKLtoKisekae.h
@@ -1,22 +1,18 @@
 /********************************************************************************
-** Form generated from reading UI file 'KKLtoKisekaeGKTCun.ui'
+** Form generated from reading UI file 'KKLtoKisekaeBaamfA.ui'
 **
 ** Created by: Qt User Interface Compiler version 6.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
 
-#ifndef KKLTOKISEKAEGKTCUN_H
-#define KKLTOKISEKAEGKTCUN_H
+#ifndef KKLTOKISEKAEBAAMFA_H
+#define KKLTOKISEKAEBAAMFA_H
 
 #include <QtCore/QVariant>
-#include <QtGui/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenu>
-#include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolButton>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
@@ -28,12 +24,6 @@ public:
     QWidget *centralwidget;
     QWidget *verticalLayoutWidget;
     QVBoxLayout *verticalLayout;
-    QToolButton *toolButton;
-    QToolButton *toolButton_2;
-    QToolButton *toolButton_3;
-    QMenuBar *menubar;
-    QMenu *menuExit;
-    QMenu *menuFile;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *KKLtoKisekae)
@@ -47,50 +37,14 @@ public:
         centralwidget->setObjectName("centralwidget");
         verticalLayoutWidget = new QWidget(centralwidget);
         verticalLayoutWidget->setObjectName("verticalLayoutWidget");
-        verticalLayoutWidget->setGeometry(QRect(0, -1, 801, 551));
+        verticalLayoutWidget->setGeometry(QRect(10, 10, 781, 551));
         verticalLayout = new QVBoxLayout(verticalLayoutWidget);
         verticalLayout->setObjectName("verticalLayout");
-        verticalLayout->setSizeConstraint(QLayout::SizeConstraint::SetDefaultConstraint);
         verticalLayout->setContentsMargins(0, 0, 0, 0);
-        toolButton = new QToolButton(verticalLayoutWidget);
-        toolButton->setObjectName("toolButton");
-        QSizePolicy sizePolicy(QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Fixed);
-        sizePolicy.setHorizontalStretch(0);
-        sizePolicy.setVerticalStretch(0);
-        sizePolicy.setHeightForWidth(toolButton->sizePolicy().hasHeightForWidth());
-        toolButton->setSizePolicy(sizePolicy);
-
-        verticalLayout->addWidget(toolButton);
-
-        toolButton_2 = new QToolButton(verticalLayoutWidget);
-        toolButton_2->setObjectName("toolButton_2");
-        sizePolicy.setHeightForWidth(toolButton_2->sizePolicy().hasHeightForWidth());
-        toolButton_2->setSizePolicy(sizePolicy);
-
-        verticalLayout->addWidget(toolButton_2);
-
-        toolButton_3 = new QToolButton(verticalLayoutWidget);
-        toolButton_3->setObjectName("toolButton_3");
-        sizePolicy.setHeightForWidth(toolButton_3->sizePolicy().hasHeightForWidth());
-        toolButton_3->setSizePolicy(sizePolicy);
-
-        verticalLayout->addWidget(toolButton_3);
-
         KKLtoKisekae->setCentralWidget(centralwidget);
-        menubar = new QMenuBar(KKLtoKisekae);
-        menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 800, 22));
-        menuExit = new QMenu(menubar);
-        menuExit->setObjectName("menuExit");
-        menuFile = new QMenu(menubar);
-        menuFile->setObjectName("menuFile");
-        KKLtoKisekae->setMenuBar(menubar);
         statusbar = new QStatusBar(KKLtoKisekae);
         statusbar->setObjectName("statusbar");
         KKLtoKisekae->setStatusBar(statusbar);
-
-        menubar->addAction(menuExit->menuAction());
-        menubar->addAction(menuFile->menuAction());
 
         retranslateUi(KKLtoKisekae);
 
@@ -100,11 +54,6 @@ public:
     void retranslateUi(QMainWindow *KKLtoKisekae)
     {
         KKLtoKisekae->setWindowTitle(QCoreApplication::translate("KKLtoKisekae", "KKLtoKisekae", nullptr));
-        toolButton->setText(QCoreApplication::translate("KKLtoKisekae", "1", nullptr));
-        toolButton_2->setText(QCoreApplication::translate("KKLtoKisekae", "2", nullptr));
-        toolButton_3->setText(QCoreApplication::translate("KKLtoKisekae", "3", nullptr));
-        menuExit->setTitle(QCoreApplication::translate("KKLtoKisekae", "File", nullptr));
-        menuFile->setTitle(QCoreApplication::translate("KKLtoKisekae", "Exit", nullptr));
     } // retranslateUi
 
 };
@@ -115,4 +64,4 @@ namespace Ui {
 
 QT_END_NAMESPACE
 
-#endif // KKLTOKISEKAEGKTCUN_H
+#endif // KKLTOKISEKAEBAAMFA_H

2d274dd83b209306fd0730426493792034fb3d73
Gabriel Misajlovski <62955382+Ebagigi@users.noreply.github.com>
2024-05-06 14:27:13 -0400

    Finished Main Window UI (Not Yet Functional)

--- ui_KKLtoKisekae.h
+++ ui_KKLtoKisekae.h
@@ -1,31 +1,63 @@
 /********************************************************************************
-** Form generated from reading UI file 'KKLtoKisekaeysMJBU.ui'
+** Form generated from reading UI file 'KKLtoKisekaeTolXFk.ui'
 **
 ** Created by: Qt User Interface Compiler version 6.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
 
-#ifndef KKLTOKISEKAEYSMJBU_H
-#define KKLTOKISEKAEYSMJBU_H
+#ifndef KKLTOKISEKAETOLXFK_H
+#define KKLTOKISEKAETOLXFK_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
+#include <QObject>
 
 QT_BEGIN_NAMESPACE
 
-class Ui_KKLtoKisekae
-{
-public:
+class Ui_KKLtoKisekae {
+private:
+    QAction *actionHelp;
+    QAction *actionAbout;
     QWidget *centralwidget;
     QWidget *verticalLayoutWidget;
     QVBoxLayout *verticalLayout;
+    QHBoxLayout *loadButtonHorizontalLayout;
+    QPushButton *loadPushButton;
+    QFrame *frame;
+    QWidget *formLayoutWidget_2;
+    QFormLayout *directoryFormLayout;
+    QLabel *pathNameLabel;
+    QLabel *loadPathLabel;
+    QHBoxLayout *fileContentsHorizontalLayout;
+    QTextEdit *kklCodeTextEdit;
+    QHBoxLayout *convertButtonHorizontalLayout;
+    QPushButton *convertPushButton;
+    QHBoxLayout *horizontalLayout_3;
+    QTextEdit *kisekaeCodeTextEdit;
+    QHBoxLayout *saveButtonHorizontalLayout;
+    QPushButton *savePushButton;
+    QGridLayout *exitButtonGridLayout;
+    QPushButton *exitPushButton;
     QStatusBar *statusbar;
+    QMenuBar *menuBar;
+    QMenu *menuFile;
 
+public:
     void setupUi(QMainWindow *KKLtoKisekae)
     {
         if (KKLtoKisekae->objectName().isEmpty())
@@ -34,6 +66,10 @@ public:
         KKLtoKisekae->setMinimumSize(QSize(600, 800));
         KKLtoKisekae->setMaximumSize(QSize(600, 800));
         KKLtoKisekae->setBaseSize(QSize(600, 800));
+        actionHelp = new QAction(KKLtoKisekae);
+        actionHelp->setObjectName("actionHelp");
+        actionAbout = new QAction(KKLtoKisekae);
+        actionAbout->setObjectName("actionAbout");
         centralwidget = new QWidget(KKLtoKisekae);
         centralwidget->setObjectName("centralwidget");
         verticalLayoutWidget = new QWidget(centralwidget);
@@ -42,10 +78,160 @@ public:
         verticalLayout = new QVBoxLayout(verticalLayoutWidget);
         verticalLayout->setObjectName("verticalLayout");
         verticalLayout->setContentsMargins(0, 0, 0, 0);
+        loadButtonHorizontalLayout = new QHBoxLayout();
+        loadButtonHorizontalLayout->setObjectName("loadButtonHorizontalLayout");
+        loadPushButton = new QPushButton(verticalLayoutWidget);
+        loadPushButton->setObjectName("loadPushButton");
+        QSizePolicy sizePolicy(QSizePolicy::Policy::Fixed, QSizePolicy::Policy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(loadPushButton->sizePolicy().hasHeightForWidth());
+        loadPushButton->setSizePolicy(sizePolicy);
+
+        loadButtonHorizontalLayout->addWidget(loadPushButton);
+
+
+        verticalLayout->addLayout(loadButtonHorizontalLayout);
+
+        frame = new QFrame(verticalLayoutWidget);
+        frame->setObjectName("frame");
+        QSizePolicy sizePolicy1(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(frame->sizePolicy().hasHeightForWidth());
+        frame->setSizePolicy(sizePolicy1);
+        frame->setMinimumSize(QSize(0, 50));
+        frame->setMaximumSize(QSize(16777215, 24));
+        frame->setBaseSize(QSize(0, 24));
+        frame->setFrameShape(QFrame::Shape::StyledPanel);
+        frame->setFrameShadow(QFrame::Shadow::Raised);
+        formLayoutWidget_2 = new QWidget(frame);
+        formLayoutWidget_2->setObjectName("formLayoutWidget_2");
+        formLayoutWidget_2->setGeometry(QRect(10, 10, 579, 54));
+        directoryFormLayout = new QFormLayout(formLayoutWidget_2);
+        directoryFormLayout->setObjectName("directoryFormLayout");
+        directoryFormLayout->setRowWrapPolicy(QFormLayout::RowWrapPolicy::WrapLongRows);
+        directoryFormLayout->setLabelAlignment(Qt::AlignmentFlag::AlignLeading|Qt::AlignmentFlag::AlignLeft|Qt::AlignmentFlag::AlignTop);
+        directoryFormLayout->setFormAlignment(Qt::AlignmentFlag::AlignHCenter|Qt::AlignmentFlag::AlignTop);
+        directoryFormLayout->setVerticalSpacing(7);
+        directoryFormLayout->setContentsMargins(0, 0, 0, 0);
+        pathNameLabel = new QLabel(formLayoutWidget_2);
+        pathNameLabel->setObjectName("pathNameLabel");
+        pathNameLabel->setEnabled(false);
+        sizePolicy1.setHeightForWidth(pathNameLabel->sizePolicy().hasHeightForWidth());
+        pathNameLabel->setSizePolicy(sizePolicy1);
+        pathNameLabel->setMinimumSize(QSize(0, 24));
+        pathNameLabel->setMaximumSize(QSize(16777215, 24));
+        pathNameLabel->setBaseSize(QSize(0, 24));
+        pathNameLabel->setAlignment(Qt::AlignmentFlag::AlignCenter);
+
+        directoryFormLayout->setWidget(0, QFormLayout::LabelRole, pathNameLabel);
+
+        loadPathLabel = new QLabel(formLayoutWidget_2);
+        loadPathLabel->setObjectName("loadPathLabel");
+        loadPathLabel->setEnabled(false);
+        sizePolicy1.setHeightForWidth(loadPathLabel->sizePolicy().hasHeightForWidth());
+        loadPathLabel->setSizePolicy(sizePolicy1);
+        loadPathLabel->setMinimumSize(QSize(0, 24));
+        loadPathLabel->setMaximumSize(QSize(16777215, 24));
+        loadPathLabel->setBaseSize(QSize(0, 24));
+
+        directoryFormLayout->setWidget(0, QFormLayout::FieldRole, loadPathLabel);
+
+
+        verticalLayout->addWidget(frame);
+
+        fileContentsHorizontalLayout = new QHBoxLayout();
+        fileContentsHorizontalLayout->setObjectName("fileContentsHorizontalLayout");
+        kklCodeTextEdit = new QTextEdit(verticalLayoutWidget);
+        kklCodeTextEdit->setObjectName("kklCodeTextEdit");
+        kklCodeTextEdit->setEnabled(true);
+        QSizePolicy sizePolicy2(QSizePolicy::Policy::MinimumExpanding, QSizePolicy::Policy::Fixed);
+        sizePolicy2.setHorizontalStretch(0);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(kklCodeTextEdit->sizePolicy().hasHeightForWidth());
+        kklCodeTextEdit->setSizePolicy(sizePolicy2);
+        kklCodeTextEdit->setAutoFillBackground(true);
+        kklCodeTextEdit->setReadOnly(false);
+
+        fileContentsHorizontalLayout->addWidget(kklCodeTextEdit);
+
+
+        verticalLayout->addLayout(fileContentsHorizontalLayout);
+
+        convertButtonHorizontalLayout = new QHBoxLayout();
+        convertButtonHorizontalLayout->setObjectName("convertButtonHorizontalLayout");
+        convertPushButton = new QPushButton(verticalLayoutWidget);
+        convertPushButton->setObjectName("convertPushButton");
+        convertPushButton->setEnabled(false);
+        sizePolicy.setHeightForWidth(convertPushButton->sizePolicy().hasHeightForWidth());
+        convertPushButton->setSizePolicy(sizePolicy);
+
+        convertButtonHorizontalLayout->addWidget(convertPushButton);
+
+
+        verticalLayout->addLayout(convertButtonHorizontalLayout);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName("horizontalLayout_3");
+        kisekaeCodeTextEdit = new QTextEdit(verticalLayoutWidget);
+        kisekaeCodeTextEdit->setObjectName("kisekaeCodeTextEdit");
+        kisekaeCodeTextEdit->setEnabled(false);
+        QSizePolicy sizePolicy3(QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Fixed);
+        sizePolicy3.setHorizontalStretch(0);
+        sizePolicy3.setVerticalStretch(0);
+        sizePolicy3.setHeightForWidth(kisekaeCodeTextEdit->sizePolicy().hasHeightForWidth());
+        kisekaeCodeTextEdit->setSizePolicy(sizePolicy3);
+        kisekaeCodeTextEdit->setAutoFillBackground(true);
+        kisekaeCodeTextEdit->setReadOnly(true);
+
+        horizontalLayout_3->addWidget(kisekaeCodeTextEdit);
+
+
+        verticalLayout->addLayout(horizontalLayout_3);
+
+        saveButtonHorizontalLayout = new QHBoxLayout();
+        saveButtonHorizontalLayout->setObjectName("saveButtonHorizontalLayout");
+        savePushButton = new QPushButton(verticalLayoutWidget);
+        savePushButton->setObjectName("savePushButton");
+        savePushButton->setEnabled(false);
+        sizePolicy.setHeightForWidth(savePushButton->sizePolicy().hasHeightForWidth());
+        savePushButton->setSizePolicy(sizePolicy);
+
+        saveButtonHorizontalLayout->addWidget(savePushButton);
+
+
+        verticalLayout->addLayout(saveButtonHorizontalLayout);
+
+        exitButtonGridLayout = new QGridLayout();
+        exitButtonGridLayout->setObjectName("exitButtonGridLayout");
+        exitButtonGridLayout->setSizeConstraint(QLayout::SizeConstraint::SetDefaultConstraint);
+        exitPushButton = new QPushButton(verticalLayoutWidget);
+        exitPushButton->setObjectName("exitPushButton");
+        sizePolicy.setHeightForWidth(exitPushButton->sizePolicy().hasHeightForWidth());
+        exitPushButton->setSizePolicy(sizePolicy);
+        exitPushButton->setAutoFillBackground(false);
+        exitPushButton->setAutoDefault(false);
+
+        exitButtonGridLayout->addWidget(exitPushButton, 0, 0, 1, 1);
+
+
+        verticalLayout->addLayout(exitButtonGridLayout);
+
         KKLtoKisekae->setCentralWidget(centralwidget);
         statusbar = new QStatusBar(KKLtoKisekae);
         statusbar->setObjectName("statusbar");
         KKLtoKisekae->setStatusBar(statusbar);
+        menuBar = new QMenuBar(KKLtoKisekae);
+        menuBar->setObjectName("menuBar");
+        menuBar->setGeometry(QRect(0, 0, 600, 22));
+        menuFile = new QMenu(menuBar);
+        menuFile->setObjectName("menuFile");
+        KKLtoKisekae->setMenuBar(menuBar);
+
+        menuBar->addAction(menuFile->menuAction());
+        menuFile->addAction(actionHelp);
+        menuFile->addAction(actionAbout);
 
         retranslateUi(KKLtoKisekae);
 
@@ -55,6 +241,23 @@ public:
     void retranslateUi(QMainWindow *KKLtoKisekae)
     {
         KKLtoKisekae->setWindowTitle(QCoreApplication::translate("KKLtoKisekae", "KKLtoKisekae", nullptr));
+        actionHelp->setText(QCoreApplication::translate("KKLtoKisekae", "Help", nullptr));
+#if QT_CONFIG(shortcut)
+        actionHelp->setShortcut(QCoreApplication::translate("KKLtoKisekae", "F1", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionAbout->setText(QCoreApplication::translate("KKLtoKisekae", "About", nullptr));
+#if QT_CONFIG(shortcut)
+        actionAbout->setShortcut(QCoreApplication::translate("KKLtoKisekae", "F3", nullptr));
+#endif // QT_CONFIG(shortcut)
+        loadPushButton->setText(QCoreApplication::translate("KKLtoKisekae", "LOAD KKL FILE", nullptr));
+        pathNameLabel->setText(QCoreApplication::translate("KKLtoKisekae", "Directory: ", nullptr));
+        loadPathLabel->setText(QCoreApplication::translate("KKLtoKisekae", "Please select a file to import from your computer or paste your KKL code in the box below.", nullptr));
+        kklCodeTextEdit->setPlaceholderText(QCoreApplication::translate("KKLtoKisekae", "KKL Code will be displayed here (Paste KKL Code here if not loading txt file)...", nullptr));
+        convertPushButton->setText(QCoreApplication::translate("KKLtoKisekae", "CONVERT KKL TO KISEKAE", nullptr));
+        kisekaeCodeTextEdit->setPlaceholderText(QCoreApplication::translate("KKLtoKisekae", "New Kisekae Code will be displayed here...", nullptr));
+        savePushButton->setText(QCoreApplication::translate("KKLtoKisekae", "SAVE KISEKAE CODE TO COMPUTER", nullptr));
+        exitPushButton->setText(QCoreApplication::translate("KKLtoKisekae", "EXIT", nullptr));
+        menuFile->setTitle(QCoreApplication::translate("KKLtoKisekae", "File", nullptr));
     } // retranslateUi
 
 };
@@ -65,4 +268,4 @@ namespace Ui {
 
 QT_END_NAMESPACE
 
-#endif // KKLTOKISEKAEYSMJBU_H
+#endif // KKLTOKISEKAETOLXFK_H
